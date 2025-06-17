4b5330df1ad7116a637d6cf75a7ecd57e4f81d21
wbartholomay <willbartholomay@gmail.com>
2025-06-17 11:14:17 -0400

    updated post user to include password

--- handler_users.go
+++ handler_users.go
@@ -3,11 +3,15 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/wbartholomay/chirpy/internal/auth"
+	"github.com/wbartholomay/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) CreateUserHandler (w http.ResponseWriter, req *http.Request) error{
 	type params struct {
 		Email string `json:"email"`
+		Password string `json:"password"`
 	}
 
 	decoder := json.NewDecoder(req.Body)
@@ -17,8 +21,18 @@ func (cfg *apiConfig) CreateUserHandler (w http.ResponseWriter, req *http.Reques
 		return getDefaultApiError(err)
 	}
 
-	dbUser, err := cfg.db.CreateUser(req.Context(), reqParams.Email)
+	hashedPassword, err := auth.HashPassword(reqParams.Password)
 	if err != nil {
+		return getDefaultApiError(err)
+	}
+
+	dbParams := database.CreateUserParams{
+		Email: reqParams.Email,
+		HashedPassword: hashedPassword,
+	}
+	
+	dbUser, err := cfg.db.CreateUser(req.Context(), dbParams)
+	if err != nil{
 		return getDefaultApiError(err)
 	}
 
